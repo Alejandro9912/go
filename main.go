ad1044fb64db21b8efcfb94235fd6b517a5921d5
Juan Calderon <juanalcarios@gmail.com>
2023-07-07 12:43:01 -0500

    Nuevas variables

--- main.go
+++ main.go
@@ -5,7 +5,11 @@ import (
 	"github.com/Alejandro9912/go/variables"
 )
 
-func main(){
-	fmt.Println("Hola Mundo")
+func main() {
+	/* fmt.Println("Hola Mundo")
 	variables.MuestroEnteros()
+	variables.RestoVariables() */
+	estado, texto := variables.ConviertoTexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
